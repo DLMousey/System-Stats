aada97c740bdac7d375773d281c8d2ccc176a385
DLMousey <tom@deadlyllama.com>
2019-11-10 04:21:04 +0000

    Initial commit

---
+++ server.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"regexp"
+	"tcp-server/lib"
+	"time"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	l, err := net.Listen(CONN_TYPE, CONN_HOST + ":" + CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+
+	for { // "while"
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequest(conn) // Handle the request in a new goroutine
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+
+	if err != nil {
+		fmt.Println("Error reading:" , err.Error())
+	}
+
+
+	entity := string(buf)
+
+	reg, err := regexp.Compile("[^A-Za-z0-9]+")
+	if err != nil {
+		fmt.Println("Failed to compile regex")
+	}
+
+	var sanitisedEntity = reg.ReplaceAllString(entity, "")
+	switch sanitisedEntity {
+	case "cpu":
+		idle0, total0 := lib.CpuLoad()
+		time.Sleep(3 * time.Second)
+		idle1, total1 := lib.CpuLoad()
+
+		idleTicks := float64(idle1 - idle0)
+		totalTicks := float64(total1 - total0)
+		cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+		strCpuUsage := fmt.Sprintf("%f", cpuUsage)
+
+		fmt.Println(strCpuUsage)
+		_, _ = conn.Write([]byte(strCpuUsage))
+		_ = conn.Close()
+	case "memory":
+		_, _ = conn.Write([]byte("Yes."))
+		_ = conn.Close()
+	default:
+		_, _ = conn.Write([]byte("Unrecognised command"))
+		_ = conn.Close()
+	}
+}
